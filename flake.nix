6f9ee5c3e3d907b89cb5e72542b57de9c2ea8a1a
Louis Dutton <louis.dutton@travelchapter.com>
2024-07-04 16:45:52 +0000

    setup hugix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Personal website and blog";
+  description = "Hugix: A Nix wrapper for Hugo";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
@@ -8,42 +8,39 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      scripts = {
-        dev = pkgs.writeShellScriptBin "dev" ''
-          hugo server -D --buildDrafts
-        '';
-      };
+      toml = pkgs.formats.toml { };
+
+      hugix =
+        cfg:
+        let
+          config = toml.generate "hugo.toml" cfg;
+        in
+        pkgs.stdenv.mkDerivation {
+          pname = "hugix";
+          version = "0.0.1";
+          src = pkgs.fetchFromGitHub {
+            owner = "nunocoracao";
+            repo = "blowfish";
+            rev = "v2.72.1";
+            hash = "sha256-Gacmm7l1Lbc4lw9bsJ1NBas+h+zBd1EeA8hioI8yfR0=";
+          };
+          dontInstall = true;
+          buildInputs = with pkgs; [ hugo ];
+          buildPhase = ''
+            hugo new site $out
+            mkdir -p $out/themes/${cfg.theme}
+            cp -r $src/* $out/themes/${cfg.theme}
+            rm $out/hugo.toml
+            cp ${config} $out/hugo.toml
+          '';
+        };
     in
     {
-      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
-        name = "website";
-        src = self;
-        buildInputs = [ pkgs.hugo ];
-        buildPhase =
-          let
-            theme = pkgs.fetchFromGitHub {
-              owner = "nunocoracao";
-              repo = "blowfish";
-              rev = "v2.72.1";
-              hash = "sha256-Gacmm7l1Lbc4lw9bsJ1NBas+h+zBd1EeA8hioI8yfR0=";
-            };
-          in
-          # bash
-          ''
-            hugo new site $out
-            mkdir $out/themes/blowfish
-            cp -r ${theme}/* $out/themes/blowfish
-            cd $out
-            hugo -t blowfish
-          '';
+      defaultPackage.${system} = hugix {
+        baseURL = "https://louisdutton.dev";
+        languageCode = "en-gb";
+        title = "Louis Dutton";
+        theme = "blowfish";
       };
-
-      devShell.${system} = pkgs.mkShell {
-        buildInputs = [
-          pkgs.hugo
-          scripts.dev
-        ];
-      };
-
     };
 }
