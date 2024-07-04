0681644ac0548cf2f6718aa8b5f7a0ec98c9ed1b
Louis Dutton <louis.dutton@travelchapter.com>
2024-07-04 12:38:51 +0000

    clean

--- flake.nix
+++ flake.nix
@@ -15,11 +15,35 @@
       };
     in
     {
+      defaultPackage.x86_64-linux = pkgs.stdenv.mkDerivation {
+        name = "website";
+        src = self;
+        buildInputs = [ pkgs.hugo ];
+        buildPhase =
+          let
+            theme = pkgs.fetchFromGitHub {
+              owner = "nunocoracao";
+              repo = "blowfish";
+              rev = "v2.72.1";
+              hash = "sha256-Gacmm7l1Lbc4lw9bsJ1NBas+h+zBd1EeA8hioI8yfR0=";
+            };
+          in
+          # bash
+          ''
+            hugo new site $out
+            mkdir $out/themes/blowfish
+            cp -r ${theme}/* $out/themes/blowfish
+            cd $out
+            hugo -t blowfish
+          '';
+      };
+
       devShell.${system} = pkgs.mkShell {
         buildInputs = [
           pkgs.hugo
           scripts.dev
         ];
       };
+
     };
 }
