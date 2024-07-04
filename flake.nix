2daf106dbd789a4d51a56cbe786c4593a47c71d1
Louis Dutton <louis.dutton@travelchapter.com>
2024-07-04 19:51:32 +0000

    output final build in result

--- flake.nix
+++ flake.nix
@@ -4,34 +4,33 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
 
   outputs =
-    { self, nixpkgs }:
+    { nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       toml = pkgs.formats.toml { };
-
       hugix =
         cfg:
         let
           config = toml.generate "hugo.toml" cfg;
-        in
-        pkgs.stdenv.mkDerivation {
-          pname = "hugix";
-          version = "0.0.1";
-          src = pkgs.fetchFromGitHub {
+          theme = pkgs.fetchFromGitHub {
             owner = "nunocoracao";
             repo = "blowfish";
             rev = "v2.72.1";
             hash = "sha256-Gacmm7l1Lbc4lw9bsJ1NBas+h+zBd1EeA8hioI8yfR0=";
           };
+        in
+        pkgs.stdenv.mkDerivation {
+          pname = "hugix";
+          version = "0.0.1";
           dontInstall = true;
+          src = theme;
           buildInputs = with pkgs; [ hugo ];
           buildPhase = ''
-            hugo new site $out
-            mkdir -p $out/themes/${cfg.theme}
-            cp -r $src/* $out/themes/${cfg.theme}
-            rm $out/hugo.toml
-            cp ${config} $out/hugo.toml
+            hugo new site tmp
+            mkdir -p tmp/themes/${cfg.theme}
+            cp -r $src/* tmp/themes/${cfg.theme}
+            hugo -s tmp -c ${config} -d $out --noBuildLock
           '';
         };
     in
